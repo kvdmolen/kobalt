3bfe2507daabc980b59571483404bcfa18280e74
Klaas van der Molen <kvdmolen@gmail.com>
2018-12-10 21:33:30 +0100

    Update board.js

--- board.js
+++ board.js
@@ -1,24 +1,22 @@
 {
-	"name": "board",
-	"element": "div",
-	"state": {
+	name: "board",
+	element: "div",
+	state: {
 		"width": 3,
 		"height": 3
 	},
-	"children": [
+	children: [
 		{
-			"element": "div",
-			"class": "row",
-			"repeat": {
-				"array": function(){return this.state.height},
-			},
-			"children": [
+			element: "div",
+			class: "row",
+			repeat(){return this.state.height},
+			children: [
 				{
-					"element": "square",
-					"repeat": this.state.width,
-					"args": {
-						"x": function(){return this.repeat.index},
-						"y": function(){return this.parent.repeat.index}
+					element: "square",
+					repeat: this.state.width,
+					args: {
+						x(){return this.repeat.index},
+						y(){return this.parent.repeat.index}
 					}
 				}
 			]
