1ac149060afdccd4fdf62d603be2239fd828b923
Klaas van der Molen <kvdmolen@gmail.com>
2018-12-10 22:04:22 +0100

    Update game.js

--- game.js
+++ game.js
@@ -7,35 +7,36 @@
 		player() => {Math.round(Math.random()) + 1}
 	},
 	children: [
-	{
-		element: "h1",
-		class: "game-title",
-		content: "Tic Tac Toe"
-	},
-	{
-		element: "div",
-		class: "game-player",
-		content() => {"Current player: " + this.state.player}
-	},
-	{
-		element: "board"
-	},
-	{
-		element: "div",
-		class: "game-winner",
-		content() => {"And the winner is: " + this.state.winner}
-	},
-	{
-		element: "mybutton",
-		class: "newgame",
-		if() => {return this.state.winner > 0},
-		props: {
-			title: "New game",
-			icon: "fa fa-plus"
+		{
+			element: "h1",
+			class: "game-title",
+			content: "Tic Tac Toe"
 		},
-		click(){
-			this.setState("player", this.state.winner)
-			this.setState("winner", 0)
+		{
+			element: "div",
+			class: "game-player",
+			content() => {"Current player: " + this.state.player}
+		},
+		{
+			element: "board"
+		},
+		{
+			element: "div",
+			class: "game-winner",
+			content() => {"And the winner is: " + this.state.winner}
+		},
+		{
+			element: "mybutton",
+			class: "newgame",
+			if() => {return this.state.winner > 0},
+			props: {
+				title: "New game",
+				icon: "fa fa-plus"
+			},
+			click(){
+				this.setState("player", this.state.winner)
+				this.setState("winner", 0)
+			}
 		}
-	}]
+	]
 }
